5104498201377ea34582fa15b7a2821bcb79b462
Joshua Oloton <olotonjoshua@gmail.com>
2022-08-23 18:45:51 +0100

    added postorder traversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - deletes an entire binary tree
+ * binary_tree_preorder -  goes through a binary tree using pre-order traversal
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
  *
