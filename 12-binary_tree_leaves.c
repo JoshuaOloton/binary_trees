c7204326e0cf5d9657bf6d8ccfb8c8091349e924
Joshua Oloton <olotonjoshua@gmail.com>
2022-08-25 10:37:24 +0100

    added no of nodes with at least 1 child

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,6 +12,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
-		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right) + 1);
+		return (binary_tree_leaves(tree->left) +
+			binary_tree_leaves(tree->right) + 1);
 	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts the nodes in a binary tree
+ * @tree:  pointer to the root node of the tree to count the number of nodes
+ *
+ * Return: no of nodes
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left || tree->right)
+		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
+	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+}
